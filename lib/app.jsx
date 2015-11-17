d09633261033727137498918558b874abf014d9b
Daphne GRIPON <daphne.gripon@gmail.com>
2015-11-17 14:31:04 +0100

    step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -11,9 +11,16 @@ var Box = React.createClass({
     return {value: this.props.initialValue};
    },
 
+   componentWillMount: function() {
+    var currentState = this;
+    this.timer = setInterval(function(){
+      currentState.setState({value: currentState.state.value == "X" ? "O" : "X"});
+    }, 300);
+  },
+
   'render': function onRender () {
     return (
-      <button style={bStyle}>{this.state.value}</button>
+      <button onLoad={this.updateState} style={bStyle}>{this.state.value}</button>
     );
   }
 });
