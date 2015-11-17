ff08c8efd3fa14ffb8c797c777aca3ed86c44c29
Daphne GRIPON <daphne.gripon@gmail.com>
2015-11-17 13:43:16 +0100

    step 2

--- lib/app.jsx
+++ lib/app.jsx
@@ -9,9 +9,9 @@ var Box = React.createClass({
    */
   'render': function onRender () {
     return (
-      <button>X</button>
+      <button>{this.props.value}</button>
     );
   }
 });
 
-React.render(<Box/>, document.body);
+React.render(<Box value="HALLO, CAN YOU HERE MEH" />, document.body);
